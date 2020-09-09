d1142f5d9a2748fae0c98e91c3ed1580c8e0f74f
Lulu Wang <47229189+lu-lu10@users.noreply.github.com>
2020-09-08 19:29:43 -0700

    Revert "Initial commit"
    This reverts commit 463b91de2528c932d3eb7c13d1eb8ca4da1c5e08.

--- R/PARSE_package.R
+++
@@ -1,7 +0,0 @@
-#' @title Model-based clustering with Penalty Functions
-#'
-#' @description The package \pkg{PARSE} provides functions for the model-based clustering with multiple penalty functions including APL1, APFP and PARSE.
-
-#' @docType package
-#' @name PARSE
-NULL
